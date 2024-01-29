268db5888db0b19fe346b4553f4ba920fe78ae97
추교윤 <chu.gyoyoon@mkinternet.com>
2024-01-29 16:34:57 +0900

    2024-01-29 - 공지 게시글 등록 기능 완료 - 권한에 따른 게시글 view 리팩토링 완료

--- mk-board/Controller/AccessController.php
+++ mk-board/Controller/AccessController.php
@@ -69,6 +69,7 @@ class AccessController extends BaseController{
         $commonUserImpossibleUrl = [
             'user/manage',
             'post/manage',
+            'user/read',
         ];
 
 
