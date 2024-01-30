b4727726786e366259e939b0946e4e26ce09f073
추교윤 <chu.gyoyoon@mkinternet.com>
2024-01-30 14:04:47 +0900

    2024-01-30 - 로그DB 생성 - 로그 Model 구현 - 로깅데이터 추출해오는 trait 메소드 구현 - 각 컨트롤러 메소드에 로깅메소드 주입 완료

--- mk-board/Route/CommentRoute.php
+++ mk-board/Route/CommentRoute.php
@@ -7,15 +7,20 @@ use Controller\CommentController;
 class CommentRoute extends BaseRoute {
     function routing($url): bool
     {
-        // TODO: Implement routing() method.
+        
         $CommentController = new CommentController();
 
+        // 댓글 생성 요청
         if($this->routeCheck($url, "comment/create", "POST")) {
             $CommentController->create();
             return true;
+            
+        // 댓글 생성 요청
         } else if($this->routeCheck($url, "comment/update", "POST")) {
             $CommentController->update();
             return true;
+            
+        // 댓글 삭제 요청
         } else if($this->routeCheck($url, "comment/delete", "POST")) {
             $CommentController->delete();
             return true;
--- mk-board/Route/FileRoute.php
+++ mk-board/Route/FileRoute.php
@@ -8,18 +8,27 @@ class FileRoute extends BaseRoute {
     {
         $FileController = new FileController();
 
+        // 파일 서버 폴더에 업로드 요청
         if($this->routeCheck($url, "file/upload", "POST")) {
             $FileController->fileUpload();
             return true;
+            
+        // 파일 DB 생성 요청
         } else if($this->routeCheck($url, "file/create", "POST")) {
             $FileController->create();
             return true;
+            
+        // 파일 데이터와 해당 게시글과 연결 요청
         } else if($this->routeCheck($url, "file/connect", "POST")) {
             $FileController->connectFileWithPost();
             return true;
+            
+        // 파일 다운로드 요청
         } else if($this->routeCheck($url, "file/download", "POST")) {
             $FileController->download();
             return true;
+            
+        // 파일 삭제 요청
         } else if($this->routeCheck($url, "file/delete", "POST")) {
             $FileController->delete();
             return true;
